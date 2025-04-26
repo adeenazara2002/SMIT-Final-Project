94602a6092b8cb452be443190dbb774fc29c3a51
adeenazara2002 <adinazara2001@gmail.com>
2025-04-26 17:28:55 +0500

    frontend

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flx-row border border-gray-400'>
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
       {/* Hero Left Side */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
         <div className='text-[#414141]'>
@@ -17,6 +18,8 @@ const Hero = () => {
             </div>
         </div>
       </div>
+      {/* Hero Right Side */}
+      <img className='w-full sm:w-1/2' src={assets.hero_img} alt=''></img>
     </div>
   )
 }
